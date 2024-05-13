e72f28ef9759da27ca1bb65f9bb84d1379ec3bee
MUKESHBAGAL <2022bit039@sggs.ac.in>
2024-05-13 15:01:08 +0530

    Index of Program

---
+++ 01INDEX_OF_PROGRAM.c
@@ -0,0 +1,70 @@
+                                                                            **INDEX_OF_PROGRAM**
+
+# ARTRAY IN DATA STRUCTURE
+	1.Transversal Of Array
+	2.Update Array Element
+	3.Insert Element at Zero index
+	4.Insert Element at any index
+	5.Delete array element
+---->Total Program == 5
+
+# LINKED LIST IN DATA STRUCTURE
+	1.Transversal of Linked List
+	2.Print Last Node of Linked List
+	3.print Second Last node
+	4.Count Number Of Node In Linked List
+	5.print Alternate Node
+	6.Check Given Node Present Or Not
+	7.Print Middle Node of Linked List
+	8.Print only Even node of Linked List
+	9.Print Only Odd node of Linked List
+	10.Write a Program For print Prime Node in Linked List
+	11.Update Node Of Linked List
+	12.Update 1st Element of Linked List
+	13.Update Last Node of linked List
+	14.Update Any Node of linked List
+	15.Insert Node At 1st
+	16.Insert Node At last 
+	17.Insert node After Any Node
+	18.Delete 1st node
+	19.Delete Last node
+	20.Delete Any Node In Linked List
+	21.Merge Sorted Linked List in Unsorted form
+	22.Merge Sorted Linked List in Sorted form
+	23.Check Intersection Point In Linked List(Two Linked list intersect to each other or not and print This Node)
+	24.Reverse Linked List 
+	25.Count Number of Duplicate Node
+	26.Delete Duplicate Node from Linked List
+	27.Segregate Even Odd Node in Linked list
+	28.Transversal And Display of Doubly  Linked List
+	29.Insert New Node at any location in Doubly Linked List
+	30.Delete any node in doubly Linked List
+	31.Transversal And Display of Circular Linked List
+	32.Transversal And Display of  Doubly Circular Linked List
+	
+# STACK AND QUEUE IN DATA STRUCTURE 
+	1.Implement Stack Using Linked List And Push and Pop Operation
+	2.Program For The Implement Two Stack
+
+# TREE
+
+	1.Program For tree traversal preorder in Binary Search Tree
+	2.Program For tree traversal Inorder in Binary Search Tree
+	3.Program For tree traversal Inorder in Binary Search Tree
+	4.Program For Count Number of Node in Binary Search Tree
+	5.Write a Program For a Count Leaf Node in Tree
+	6.Write a Program For a Count Non Leaf Node in Tree
+	7.Write a Program For a Find Maximum node in tree
+	8.Write a Program For print Node form a K Distance
+	9.Write a code For Satisfy Children Sum Property
+	10.Write A code For Search Element In Tree
+## TOTAL CODE  ##
+	
+	CHAPTER NAME                                    TOTAL CODE
+	
+	I. ARRAY                                            5
+	II.LINKED LIST                                      32
+	III.STACK AND QUEUE                                 2
+	IV .TREE                                            10
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	TOTAL                                               49
